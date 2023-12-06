97faf49feab748ab1581391eab17cc646900c5cf
AlexKangas <alexkang@hotmail.se>
2023-12-06 15:18:52 +0100

    fix: Add input gt file and output folder as arguments

--- datasets/filter_gt.py
+++ datasets/filter_gt.py
@@ -1,22 +1,54 @@
 import re
 import string
-batch_max_length = 25
-character  = string.printable[:-6] #'0123456789abcdefghijklmnopqrstuvwxyz'
-out_of_char = f'[^{character}]'
-filtered_gt = open('IAM_test_lmdb/filtered_gt.txt', 'w')
-with open('iam_rwth_partitions/RWTH.iam_word_gt_final.test.thresh.txt', 'r') as f:
-    lines = f.readlines()
-    words = [line.strip().split(' ',1)[1] for line in lines]
-    image_names = [line.strip().split(' ',1)[0] for line in lines]
+import argparse
 
-    for image_name, word in zip(image_names, words):
+def filter_gt(args):
 
-        if re.search(out_of_char, word.lower()) is not None:
-            print(word)
+    batch_max_length = 25
+    character  = string.printable[:-6] #'0123456789abcdefghijklmnopqrstuvwxyz'
+    out_of_char = f'[^{character}]'
+    filtered_gt = open(f'{args.out}/filtered_gt.txt', 'w')
+    with open(f'{args.path_to_gt}', 'r') as f:
+        lines = f.readlines()
+        words = [line.strip().split(' ',1)[1] for line in lines]
+        image_names = [line.strip().split(' ',1)[0] for line in lines]
 
-        if not len(word) > batch_max_length and re.search(out_of_char, word.lower()) is None:
-           filtered_gt.write(f'{image_name} {word}\n')
+        for image_name, word in zip(image_names, words):
 
+            if re.search(out_of_char, word.lower()) is not None:
+                print(word)
 
+            if not len(word) > batch_max_length and re.search(out_of_char, word.lower()) is None:
+                filtered_gt.write(f'{image_name} {word}\n')
 
-filtered_gt.close()
+    filtered_gt.close()
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Argument parser")
+
+    parser.add_argument(
+        "--path_to_gt",
+        required=True,
+        help="Path to gt.txt.")
+
+    parser.add_argument(
+        "--out",
+        required=True,
+        help="Prefix of the directory to contain output.")
+
+    parser.add_argument(
+        "--verbose",
+        type=int,
+        default=0,
+        required=False,
+        help="Print debug info. 0 - important messages only, 1 - more details, Default: 0.")
+
+    args = parser.parse_args()
+    if args.verbose >= 1:
+        print(f"Parsed arguments: {args}")
+
+    return args
+
+if __name__ == '__main__':
+    args = parse_args()
+    filter_gt(args)
